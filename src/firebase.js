c64dca9af2a04e7d5786f06561c70d2af2940658
Yash <yashvedre04@gmail.com>
2026-03-06 10:02:51 +0530

    message

--- src/firebase.js
+++ src/firebase.js
@@ -5,28 +5,20 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQvZej-SH6RgQ7hIgvxO8RP66iu66gnlM",
-  authDomain: "alumni-8b352.firebaseapp.com",
-  projectId: "alumni-8b352",
-  storageBucket: "alumni-8b352.appspot.com", 
-  messagingSenderId: "854261512420",
-  appId: "1:854261512420:web:4c670f9e1397aa77f214f1",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
-
-// 🔐 Auth
 export const auth = getAuth(app);
-
-// Secondary Auth (for admin use)
 export const secondaryAuth = getAuth(
   initializeApp(firebaseConfig, "Secondary")
 );
 
-// 🔥 Firestore
 export const db = getFirestore(app);
-
-// 🔥 Firebase Storage (FOR IMAGE UPLOAD)
 export const storage = getStorage(app);
-
 export const googleProvider = new GoogleAuthProvider();
